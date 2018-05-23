a09656d188d76790c11af4b66f58c970cb8009d1
Nick Tan <nctan1002@gmail.com>
2018-05-23 03:15:27 -0400

    Added message box functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,25 @@ $(document).ready(function() {
 
     $(".choice").on("click", function() {
         let yourChoice = $(this).data("choice");
+        database.ref().push();
         console.log(yourChoice);
+
+    });
+
+    $(".submitMessage").on("click", function(event) {
+        event.preventDefault();
+        let message = $("#message").val();
+        let name = $("#name").val();
+        database.ref("messages").push({
+            name: name,
+            message: message
+        });
+    });
+
+    database.ref("messages").on("child_added", function(snapshot) {
+        console.log(snapshot.val());
+        let oldMessages = $("#messages").text();
+        $("#messages").text(`${oldMessages}\n${snapshot.val().name}: ${snapshot.val().message}`)
     });
 
 });
--- assets/js/initialize.js
+++ assets/js/initialize.js
@@ -1,12 +1,12 @@
 // Initialize Firebase
 const config = {
-    apiKey: "AIzaSyBLH2YBIZXuUZTOKvK2Go2EzUO0JBL1SnU",
-    authDomain: "rps-multiplayer-daefd-65724.firebaseapp.com",
-    databaseURL: "https://rps-multiplayer-daefd-65724.firebaseio.com",
-    projectId: "rps-multiplayer-daefd",
-    storageBucket: "",
-    messagingSenderId: "854965808795"
-  };
+  apiKey: "AIzaSyBLH2YBIZXuUZTOKvK2Go2EzUO0JBL1SnU",
+  authDomain: "rps-multiplayer-daefd.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-daefd.firebaseio.com",
+  projectId: "rps-multiplayer-daefd",
+  storageBucket: "rps-multiplayer-daefd.appspot.com",
+  messagingSenderId: "854965808795"
+};
 firebase.initializeApp(config);
 const database = firebase.database();
 
